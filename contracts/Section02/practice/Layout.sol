09bf76a9c272a5948cd7753ca70dae118d097d27
y-okamoto-1113 <okamoto.yuuki.1113+github@gmail.com>
2023-03-22 02:54:03 +0900

    :construction: Completed section 2

--- contracts/Section02/practice/Layout.sol
+++ contracts/Section02/practice/Layout.sol
@@ -1,10 +1,27 @@
 // SPDX-License-Identifier: MIT
+
+/**
+ * @dev pragma定義
+ * 特定のコンパイラ機能またはチェックを有効にするために使用する
+ */
 pragma solidity ^0.8.17;
 
-import "hardhat/console.sol";
+import "../../../node_modules/hardhat/console.sol";
+// import "hardhat/console.sol"; // この書き方でも読み込めているがエディタがエラー吐く。remixではbase path設定してるから問題ないっぽい。
 
+/**
+ * @dev contract定義
+ * 1ファイルに複数定義することも可能
+ */
 contract Layout {
+    /// @dev デバッグプリントするためだけのファンクション
     function logging() external view {
+        /*
+            msg.senderはグローバル変数で、Callまたは
+            Transaction発行元のアカウントアドレス
+        */
         console.log(msg.sender);
     }
 }
+
+contract A {}
--- contracts/Section02/practice/Variables.sol
+++ contracts/Section02/practice/Variables.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.17;
 
-/** 
+/**
  * @title 変数 を学ぼう
  * Solidityには、3種類の変数がある
  * - 状態変数(State Variables)
@@ -11,12 +11,16 @@ pragma solidity ^0.8.17;
  */
 contract Variables {
     // 状態変数 : コントラクトのストレージ領域に格納される変数(SSD/HDDへの永続的な保管イメージ)
+    uint num = 1;
 
 
+    // a_はローカル変数 : データ保持期間は関数終了まで(memoryへの一時的な保管イメージ)
+    function add(uint a_) public view returns(uint) {
+        return num + a_;
+    }
 
-        // a_はローカル変数 : データ保持期間は関数終了まで(memoryへの一時的な保管イメージ)
-
-
-        //グローバル変数 : トランザクションやブロックチェーンのプロパティに関する情報を保持する特別な変数 
-
+    //グローバル変数 : トランザクションやブロックチェーンのプロパティに関する情報を保持する特別な変数
+    function getBlockTimeStamp() public view returns(uint) {
+        return block.timestamp;
+    }
 }
